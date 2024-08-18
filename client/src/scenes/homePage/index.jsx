11654ac1610ebeb0631d2a453be9f99c13aa4c90
DinareAydemirova <dinaraaydamirova@gmail.com>
2024-08-18 18:30:52 +0400

    end

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,6 +1,10 @@
 import { Box, useMediaQuery } from "@mui/material";
 import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
+import AdvertWidget from "scenes/widgets/AdvertWidgit";
+import FriendListWidget from "scenes/widgets/FriendListWidgit";
+import MyPostWidget from "scenes/widgets/MyPostWidget";
+import PostsWidget from "scenes/widgets/PostsWidget";
 import UserWidget from "scenes/widgets/UserWidget";
 
 
@@ -26,8 +30,15 @@ const HomePage = () => {
           flexBasis={isNonMobileScreens ? "42%" : undefined}
           mt={isNonMobileScreens ? undefined : "2rem"}
         >
-          
+          <MyPostWidget picturePath={picturePath}/>
+          <PostsWidget userId={_id}/>
         </Box>
+        {isNonMobileScreens && <Box flexBasis="26%">
+          <AdvertWidget/>
+          <Box m="2rem 0">
+            <FriendListWidget userId={_id}/>
+          </Box>
+          </Box>}
       </Box>
     </Box>
   );
